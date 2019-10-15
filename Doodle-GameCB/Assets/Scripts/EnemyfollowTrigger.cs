4f9c9f9df6c1145c76e286667c80673dc8672eaf
Cody Brewer <CB535114@ohio.edu>
2019-10-14 22:33:03 -0400

    Enemy Follow and Patrol
    Enemy now Patrols between an array of points and if the players trigger collides with the enemy it will begin to follow

--- Doodle-GameCB/Assets/Scripts/EnemyfollowTrigger.cs
+++ Doodle-GameCB/Assets/Scripts/EnemyfollowTrigger.cs
@@ -4,21 +4,88 @@ using UnityEngine;
 
 public class EnemyfollowTrigger : MonoBehaviour
 {
+    public float moveSpeed;
+    public Transform[] moveSpots;
+    private int _randomSpot;
+    private float _waitTime;
+    public float startWaitTime;
+
+    private bool _characterNear;
+    public Transform player;
+
+    //attempted to flip the enemy character model based on current and previous location
+    private bool _isFacingRight;
+    private float _previousXPos;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _randomSpot = Random.Range(0, moveSpots.Length);
+        _waitTime = startWaitTime;
+        _previousXPos = transform.position.x;
+        _isFacingRight = true;
     }
+
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+     
         
+
+
+
+        if(_characterNear == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            _characterNear = false;
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[_randomSpot].position, moveSpeed * Time.deltaTime);
+        }
+       
+
+
+       if (Vector2.Distance(transform.position, moveSpots[_randomSpot].position) < 0.2f)
+        {
+            if (_waitTime <= 0)
+            {
+                _randomSpot = Random.Range(0, moveSpots.Length);
+                _waitTime = startWaitTime;
+
+            }
+            else
+            {
+                _waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        _characterNear = true;
+
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _characterNear = false;
+    }
+
+    
 }
