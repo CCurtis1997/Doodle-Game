7456bfdc726cd713eb4549d7d8e9ddd3cca37030
Cody Brewer <CB535114@ohio.edu>
2019-10-17 23:05:01 -0400

    More Loot Physics and ShootDelay
    Random loot physics script currently disabled.
    Shoot delay refill is working but it won't fire a second time. have to look into it

---
+++ Doodle-GameCB/Assets/Scripts/ShootDelay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootDelay : MonoBehaviour
+{
+    public Slider shootDelayBar;
+    public float maxCoolDown = 100;
+    private float _currentCoolDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentCoolDown = maxCoolDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Doodle-GameCB/Assets/Scripts/ShootEraser.cs
+++ Doodle-GameCB/Assets/Scripts/ShootEraser.cs
@@ -1,44 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootEraser : MonoBehaviour
 {
     public GameObject eraser;
-    public float shootCooldown = 0.2f;
-    private bool _isShooting;
+    
+   
+
+    public Slider shootDelayBar;
+    public float maxCoolDown = 100;
+    private float _currentCoolDown;
+    public float refillSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        _isShooting = false;
+        _currentCoolDown = maxCoolDown;
+        shootDelayBar.value = _currentCoolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_currentCoolDown >= 0 && _currentCoolDown <= 100)
+        {
+            _currentCoolDown += Time.deltaTime * refillSpeed;
+            shootDelayBar.value = _currentCoolDown;
 
-        if (Input.GetKeyDown(KeyCode.F) && _isShooting == false)
+
+        }
+        else
+        {
+            _currentCoolDown = 100;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && _currentCoolDown > 80)
         {
             Instantiate(eraser, transform.position, Quaternion.identity);
-            _isShooting = true;
+            SetBar();
+
+            Debug.Log("firing");
+
+           
 
         }
-
-        if (_isShooting == true)
-        {
-            shootCooldown -= Time.deltaTime;
-
-            if(shootCooldown <= 0)
-            {
-                shootCooldown = 0;
-
-                if(shootCooldown == 0)
-                {
-                    _isShooting = false;
-                }
-            }
-        }
-
-        
+       
     }
+
+    private void FixedUpdate()
+    {
+
+      
+    }
+
+    private void SetBar()
+    {
+        _currentCoolDown = 0;
+        shootDelayBar.value = _currentCoolDown;
+    }
+
+ 
+
+   
 }
