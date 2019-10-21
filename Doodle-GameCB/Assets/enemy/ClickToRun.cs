1f23a9f8762bb354d0749b7d2b01ab8c71554e59
Cody Brewer <CB535114@ohio.edu>
2019-10-21 00:49:39 -0400

    Enemy Death, Patrol, and Loot Drop

---
+++ Doodle-GameCB/Assets/enemy/ClickToRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToRun : MonoBehaviour
+{
+
+    public Animator animator;
+    public bool Jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Running()
+    {
+        animator.SetTrigger("isRunning");
+    }
+
+
+}
